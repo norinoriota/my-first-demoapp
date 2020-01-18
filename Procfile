ef88509c29456f33425f75a49422edc090b198c0
masanori <msnr4kai@yahoo.co.jp>
2020-01-18 11:34:04 +0900

    My Django Girls app, first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn norinori.wsgi
---
+++ myvenv2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/msnr4kai/Desktop/キカガク/djangogirls/myvenv2/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
